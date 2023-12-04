b60bb08de25a46186f0406ad261677b3a33836e3
Sourav356 <souravplacement01@gmail.com>
2023-12-05 01:10:19 +0530

    new version

--- script.js
+++ script.js
@@ -47,3 +47,41 @@ function loadinganimation(){
     })
 }
 loadinganimation()
+
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+
+    })
+})
+
+// document.querySelector("#child1").addEventListener("mouseenter",function(){
+//     gsap.to("#cursor",{
+//         transform: "translate(-50%,-50%) scale(1)"
+        
+//     })
+
+// })
+
+// document.querySelector("#child1").addEventListener("mouseleave",function(){
+//     gsap.to("#cursor",{
+//         transform: "translate(-50%,-50%) scale(0)"
+
+//     })
+
+// })
+
+document.querySelectorAll(".child").forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        gsap.to("#cursor",{
+            transform: "translate(-50%,-50%) scale(1)"           
+        })
+    })
+    elem.addEventListener("mouseleave",function(){
+        gsap.to("#cursor",{
+            transform: "translate(-50%,-50%) scale(0)"           
+        })
+    })
+})
+
